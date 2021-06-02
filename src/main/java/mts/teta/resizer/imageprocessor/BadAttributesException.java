80e5a80a4a3e1664d643788db1c1d185c4a1a2ad
viktoria <viktoriafomiceva01696@gmail.com>
2021-06-02 17:37:29 +0300

    crop-crop realisation

--- src/main/java/mts/teta/resizer/imageprocessor/BadAttributesException.java
+++ src/main/java/mts/teta/resizer/imageprocessor/BadAttributesException.java
@@ -1,6 +1,6 @@
 package mts.teta.resizer.imageprocessor;
 
-public class BadAttributesException extends Exception{
+public class BadAttributesException extends Exception {
     public BadAttributesException(String errorMessage) {
         super(errorMessage);
     }
